15d188a0783f630e322e14fb7382173881b2fd3c
liwenqiang1990 <liwenqiang1990@gmail.com>
2023-04-17 20:49:51 +0800

    add new bilibili view function

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 import os
 import subprocess
 import pandas as pd
-import sys
+import json
 from selenium.common.exceptions import TimeoutException
 
 def parse_time(time_str):
@@ -63,28 +64,125 @@ def auto_tshark(work_path,id,t,tshark):
     duration='duration:'+str(t)
     pcap_name = os.path.join(work_path,id+'.pcap')
     option = [tshark[0],'-i',tshark[1],'-a',duration,'-F','pcap','-w',pcap_name]
-    print(option)
+    #print(option)
     p = subprocess.Popen(option,stdout=subprocess.PIPE,stderr = subprocess.PIPE)
     return p
 
-def bili_views(i,threshold,videos_list,result_path,chrome_options,tshark):
+def set_cookies(cookies_path):
+    USER_AGENT = [
+        'Opera/9.80 (Windows NT 6.1; WOW64; U; en) Presto/2.10.229 Version/11.62',
+        'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.9.168 Version/11.52',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.51 Safari/537.36 Edg/90.0.818.27'
+        'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0',
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:21.0) Gecko/20130331 Firefox/21.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0',
+    ]
+        
+    chrome_options=Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument("--disable-javascript")
+    chrome_options.add_argument('start-maximized')
+    chrome_options.add_argument('--user-agent={}'.format(USER_AGENT[2]))
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.add_argument("--disable-3d-apis")
+    
+    browser = webdriver.Chrome(options=chrome_options)
+    browser.get('https://www.bilibili.com/')
+ 
+    browser.implicitly_wait(10)
+ 
+    input() #手动登录完成后在命令行内按回车，因为我用input阻塞了
+    dictcookie = browser.get_cookies()
+    print('dictcookie:',dictcookie)
+    jsoncookie = json.dumps(dictcookie)
+    print('bilibili_cookie.json:',jsoncookie)
+    with open(cookies_path,'w') as f:
+        f.write(jsoncookie)
+    browser.close()
+
+def bili_views2(i,threshold,videos_list,result_path,cookies_path,chrome_options,tshark):
     av = videos_list.loc[i]['id']
     t = parse_time(videos_list.loc[i]['play_time'])+5 #多抓10秒包
-    if t>(threshold+10):
+    if t>(threshold+5):
         return
-    t = 60
+    
     work_path = os.path.join(result_path,str(i)+'_'+av)
     if not os.path.exists(work_path):
         os.makedirs(work_path)
 
     browser = webdriver.Chrome(options=chrome_options)
+    p = auto_tshark(work_path,av,t,tshark)
+    # 访问网页
+    url = 'https://player.bilibili.com/player.html?bvid='+av
+    start_time = time.time()
+    browser.get(url)
+    
+    browser.save_screenshot(os.path.join(work_path,'start.png'))
+    # 2倍速播放
+    # js_2 = '''document.querySelector('video').playbackRate=2'''
+    # browser.execute_script(js_2) # 执行js的方法
+    # 播放
+    try:
+        danmaku_buttom = WebDriverWait(browser, 5).until(
+                        EC.presence_of_element_located((By.XPATH, '//*[@id="bofqi"]/div/div/div[1]/div[1]/div[11]/div[2]/div[2]/div[2]/div/div[2]/div[1]/div/input'))
+                      )
+        danmaku_buttom.click()
+    except:
+        print('danmaku_button error')
+
+    videos_list.loc[i,'danmaku'] = 'off'
+    play_time = time.time()
+
+    print('第{}个视频播放成功,准备时间{}'.format(i,play_time-start_time))
+    p.communicate()
+    #获取进度条//
+
+    browser.save_screenshot(os.path.join(work_path,'end.png'))
+    end_time = time.time()
+    #return code为0表示正常tshark运行正常结束
+    tshark_state = 'return code{},tshark_time{},work_time{}\n'.format(p.returncode,t,end_time-start_time)
+    print(tshark_state)
+    videos_list.loc[i,'tshark_state']=tshark_state
+    browser.close() # 关闭当前页面
+    browser.quit() # 关闭浏览器   
+
+def bili_views(i,threshold,videos_list,result_path,cookies_path,chrome_options,tshark):
+    av = videos_list.loc[i]['id']
+    t = parse_time(videos_list.loc[i]['play_time'])+5 #多抓10秒包
+    if t>(threshold+10):
+        return
+    
+    work_path = os.path.join(result_path,str(i)+'_'+av)
+    if not os.path.exists(work_path):
+        os.makedirs(work_path)
+
+    browser = webdriver.Chrome(options=chrome_options)
+    browser.get('https://www.bilibili.com/')
+    browser.delete_all_cookies()
+    with open(cookies_path,'r') as f:
+        ListCookies = json.loads(f.read())
+    for cookie in ListCookies:
+        browser.add_cookie({
+            'domain': '.bilibili.com',
+            'name': cookie['name'],
+            'value': cookie['value'],
+            'path': '/',
+            'expires': None,
+            'httponly': False,
+        })
 
     # 访问网页
     url = videos_list.loc[i,'url']
     start_time = time.time()
     p = auto_tshark(work_path,av,t+5,tshark)
     print(url)
+
     browser.get(url)
+
+
+
+
     browser.save_screenshot(os.path.join(work_path,'start.png'))
     # 2倍速播放
     # js_2 = '''document.querySelector('video').playbackRate=2'''
